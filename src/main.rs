2574fe9a703290cfc4b78039da24e64a4493f7d4
Ибраев Ерлан <erlanibraev@gmail.com>
2026-07-24 16:22:46 +0500

    minigrep

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Проблема при разборе аргументов: {}", err);
+        eprintln!("Проблема при разборе аргументов: {}", err);
         process::exit(1);
     });
-    println!("Поиск {}", config.query);
-    println!("В файле {}", config.filename);
 
     if let Err(e) = run(config) {
-        println!("Проблема при выполнении: {}", e);
+        eprintln!("Проблема при выполнении: {}", e);
         process::exit(1);
     }
 }
